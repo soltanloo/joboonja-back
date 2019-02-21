2a8857cdb801851cef42f05e88ca82c1010ce228
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-02-21 10:03:41 +0330

    get initial data

--- src/main/java/Auctioneer.java
+++ src/main/java/Auctioneer.java
@@ -1,100 +1,100 @@
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
-
-public class Auctioneer {
-    private ObjectMapper objectMapper = new ObjectMapper();
-    private ArrayList<Project> projects = new ArrayList<Project>();
-    private ArrayList<User> users = new ArrayList<User>();
-
-    void register(String commandValue) throws IOException {
-        User newUser = this.objectMapper.readValue(commandValue, User.class);
-        if (!users.stream().anyMatch(u -> (u.getUsername().equals(newUser.getUsername()))))
-            users.add(newUser);
-        else
-            System.out.println("User \'" + newUser.getUsername() + "\' already exists.");
-    }
-
-    void addProject(String commandValue) throws IOException {
-        Project newPro = this.objectMapper.readValue(commandValue, Project.class);
-        if (!projects.stream().anyMatch(p -> (p.getTitle().equals(newPro.getTitle()))))
-            projects.add(newPro);
-        else
-            System.out.println("Project \'" + newPro.getTitle() + "\' already exists.");
-    }
-
-    void bid(String commandValue) throws IOException {
-        Bid b = this.objectMapper.readValue(commandValue, Bid.class);
-        Project project = null;
-        User user = null;
-        for (Project p:
-             projects) {
-            if (p.getTitle().equals(b.getProjectTitle())){
-                project = p;
-            }
-        }
-        for (User u:
-                users) {
-            if (u.getUsername().equals(b.getBiddingUser())){
-                user = u;
-            }
-        }
-        User finalUser = user;
-        if (project.getBids().stream().anyMatch(bid -> bid.getBiddingUser().equals(finalUser.getUsername())))
-            System.out.println("User \'" + user.getUsername() + "\' has already bidden on this project.");
-        else if (project.getBudget() >= b.getBidAmount()){
-            int containedSkills = 0;
-            for (Skill s :
-                    project.getSkills()) {
-                if (user.getSkills().stream().anyMatch(us -> (us.getName().equals(s.getName()) && us.getPoints() >= s.getPoints()))) {
-                    containedSkills++;
-                }
-            }
-            if (containedSkills == project.getSkills().size()){
-                project.addBid(b);
-                System.out.println("Bid added.");
-            }
-        }
-    }
-
-    String auction(String commandValue) throws IOException {
-        Map<String, String> project;
-        project = objectMapper.readValue(commandValue, new TypeReference<Map<String, String>>(){});
-        String winningBidder = "";
-        int maxSum = 0;
-        for (Project p :
-                projects) {
-            if (p.getTitle().equals(project.get("projectTitle"))){
-                for (Bid b :
-                        p.getBids()) {
-                    int sum = 0;
-                    String biddingUser = "";
-                    for (User u :
-                            users) {
-
-                        if (u.getUsername().equals(b.getBiddingUser())) {
-                            biddingUser = b.getBiddingUser();
-                            for (Skill s :
-                                    u.getSkills()) {
-                                for (Skill ps:
-                                        p.getSkills()) {
-                                    if (ps.getName().equals(s.getName())){
-                                        sum += 10000 * (s.getPoints() - ps.getPoints()) * (s.getPoints() - ps.getPoints());
-                                    }
-                                }
-                            }
-                        }
-                        sum += (p.getBudget() - b.getBidAmount());
-                    }
-                    if (sum > maxSum){
-                        maxSum = sum;
-                        winningBidder = biddingUser;
-                    }
-                }
-            }
-        }
-        return winningBidder;
-    }
-}
+//import com.fasterxml.jackson.core.type.TypeReference;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.Map;
+//
+//public class Auctioneer {
+//    private ObjectMapper objectMapper = new ObjectMapper();
+//    private ArrayList<Project> projects = new ArrayList<Project>();
+//    private ArrayList<User> users = new ArrayList<User>();
+//
+//    void register(String commandValue) throws IOException {
+//        User newUser = this.objectMapper.readValue(commandValue, User.class);
+//        if (!users.stream().anyMatch(u -> (u.getUsername().equals(newUser.getUsername()))))
+//            users.add(newUser);
+//        else
+//            System.out.println("User \'" + newUser.getUsername() + "\' already exists.");
+//    }
+//
+//    void addProject(String commandValue) throws IOException {
+//        Project newPro = this.objectMapper.readValue(commandValue, Project.class);
+//        if (!projects.stream().anyMatch(p -> (p.getTitle().equals(newPro.getTitle()))))
+//            projects.add(newPro);
+//        else
+//            System.out.println("Project \'" + newPro.getTitle() + "\' already exists.");
+//    }
+//
+//    void bid(String commandValue) throws IOException {
+//        Bid b = this.objectMapper.readValue(commandValue, Bid.class);
+//        Project project = null;
+//        User user = null;
+//        for (Project p:
+//             projects) {
+//            if (p.getTitle().equals(b.getProjectTitle())){
+//                project = p;
+//            }
+//        }
+//        for (User u:
+//                users) {
+//            if (u.getUsername().equals(b.getBiddingUser())){
+//                user = u;
+//            }
+//        }
+//        User finalUser = user;
+//        if (project.getBids().stream().anyMatch(bid -> bid.getBiddingUser().equals(finalUser.getUsername())))
+//            System.out.println("User \'" + user.getUsername() + "\' has already bidden on this project.");
+//        else if (project.getBudget() >= b.getBidAmount()){
+//            int containedSkills = 0;
+//            for (Skill s :
+//                    project.getSkills()) {
+//                if (user.getSkills().stream().anyMatch(us -> (us.getName().equals(s.getName()) && us.getPoint() >= s.getPoint()))) {
+//                    containedSkills++;
+//                }
+//            }
+//            if (containedSkills == project.getSkills().size()){
+//                project.addBid(b);
+//                System.out.println("Bid added.");
+//            }
+//        }
+//    }
+//
+//    String auction(String commandValue) throws IOException {
+//        Map<String, String> project;
+//        project = objectMapper.readValue(commandValue, new TypeReference<Map<String, String>>(){});
+//        String winningBidder = "";
+//        int maxSum = 0;
+//        for (Project p :
+//                projects) {
+//            if (p.getTitle().equals(project.get("projectTitle"))){
+//                for (Bid b :
+//                        p.getBids()) {
+//                    int sum = 0;
+//                    String biddingUser = "";
+//                    for (User u :
+//                            users) {
+//
+//                        if (u.getUsername().equals(b.getBiddingUser())) {
+//                            biddingUser = b.getBiddingUser();
+//                            for (Skill s :
+//                                    u.getSkills()) {
+//                                for (Skill ps:
+//                                        p.getSkills()) {
+//                                    if (ps.getName().equals(s.getName())){
+//                                        sum += 10000 * (s.getPoint() - ps.getPoint()) * (s.getPoint() - ps.getPoint());
+//                                    }
+//                                }
+//                            }
+//                        }
+//                        sum += (p.getBudget() - b.getBidAmount());
+//                    }
+//                    if (sum > maxSum){
+//                        maxSum = sum;
+//                        winningBidder = biddingUser;
+//                    }
+//                }
+//            }
+//        }
+//        return winningBidder;
+//    }
+//}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,61 @@
 import javafx.util.Pair;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Scanner;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     private static boolean isFinished = false;
 
     public static void main(String[] args) throws IOException {
-        Auctioneer auctioneer = new Auctioneer();
-        while (!isFinished) {
-            Pair<String, String> commandParts = getCommandParts();
-            String commandName = commandParts.getKey();
-            String commandData = commandParts.getValue();
+        InputStream response = new URL("http://142.93.134.194:8000/joboonja/project").openStream();
+        try (Scanner scanner = new Scanner(response)) {
+            String responseBody = scanner.useDelimiter("\\A").next();
+            System.out.println(responseBody);
+        }
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/projects", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+//        Auctioneer auctioneer = new Auctioneer();
+//        while (!isFinished) {
+//            Pair<String, String> commandParts = getCommandParts();
+//            String commandName = commandParts.getKey();
+//            String commandData = commandParts.getValue();
+//
+//            switch(commandName) {
+//                case "register":
+//                    auctioneer.register(commandData);
+//                    break;
+//                case "addProject":
+//                    auctioneer.addProject(commandData);
+//                    break;
+//                case "bid":
+//                    auctioneer.bid(commandData);
+//                    break;
+//                case "auction":
+//                    System.out.println("-> winner: " + auctioneer.auction(commandData));
+//                    isFinished = true;
+//                    break;
+//            }
+//        }
+    }
 
-            switch(commandName) {
-                case "register":
-                    auctioneer.register(commandData);
-                    break;
-                case "addProject":
-                    auctioneer.addProject(commandData);
-                    break;
-                case "bid":
-                    auctioneer.bid(commandData);
-                    break;
-                case "auction":
-                    System.out.println("-> winner: " + auctioneer.auction(commandData));
-                    isFinished = true;
-                    break;
-            }
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
         }
     }
 
