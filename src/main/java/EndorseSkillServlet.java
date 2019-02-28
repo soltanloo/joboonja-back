0fd8d3cf0d0938a4c5a48fe37ff34cece6b0b5f7
tinayz <yazdizadeh.tina@gmail.com>
2019-02-28 17:57:46 +0330

    make change in endorseSkillServlet

--- src/main/java/EndorseSkillServlet.java
+++ src/main/java/EndorseSkillServlet.java
@@ -3,12 +3,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.plaf.synth.SynthEditorPaneUI;
 import java.io.IOException;
 
 @WebServlet("/user/endorseskill")
 public class EndorseSkillServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String user_id = request.getParameter("endorser");
+        String skillName = request.getParameter("skillName");
+        //System.out.println(skillName);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/webapp/project.jsp
+++ src/main/webapp/project.jsp
@@ -5,6 +5,7 @@
   Time: 11:03
   To change this template use File | Settings | File Templates.
 --%>
+<%@ page isELIgnored="false" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <!DOCTYPE html>
