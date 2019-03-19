a8b73e7573175b661a62bb53550ea544ab81d14b
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-03-19 19:36:53 +0330

    Change bid servlet responses to json

--- src/main/java/Servlets/BidServlet.java
+++ src/main/java/Servlets/BidServlet.java
@@ -1,10 +1,12 @@
 package Servlets;
 
+import DTOs.MessageWithStatusCode;
 import Exceptions.ProjectException;
 import Exceptions.UserException;
 import Joboonja.ProjectManager;
 import Joboonja.UserManager;
 import Models.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,22 +14,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/projects/bid")
 public class BidServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        MessageWithStatusCode messageWithStatusCode = new MessageWithStatusCode();
         int bidAmount = Integer.valueOf(request.getParameter("bidAmount"));
-        String projectId = request.getParameter("projectId");
+        String projectId = request.getParameter("id");
         try {
             ProjectManager.addBidToProject(bidAmount, UserManager.getCurrentUser().getId(), projectId);
-            response.sendRedirect("/project/"+ projectId);
-        } catch (UserException | ProjectException e){
+            messageWithStatusCode.setStatusCode(200);
+            messageWithStatusCode.setMessage("Bid is submitted successfully.");
+        } catch (UserException e){
             response.setStatus(404);
-            request.setAttribute("message",
-                    e.getMessage());
-            request.getRequestDispatcher("/404.jsp").forward(request, response);
+            messageWithStatusCode.setStatusCode(404);
+            messageWithStatusCode.setMessage(e.getMessage());
+        } catch (ProjectException e){
+            response.setStatus(400);
+            messageWithStatusCode.setStatusCode(400);
+            messageWithStatusCode.setMessage(e.getMessage());
         }
-
+        String message = mapper.writeValueAsString(messageWithStatusCode);
+        out.println(message);
     }
 }
